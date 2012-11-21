6f0e10e11302e695cf3caaa3c15624a8b2856fb8
Joona Rossi <joxu@kapsi.fi>
2012-11-21 21:34:39 +0200

    ADC and struct for registers added

--- src/NES/cpu.cpp
+++ src/NES/cpu.cpp
@@ -1,42 +1,122 @@
 //implementation of 6502
 #include <stdint.h>
+#include <iostream>
 #include "cpu.hpp"
 			
-CPU::CPU(uint8_t A,
-	 uint8_t X,
-	 uint8_t Y,
-	 uint8_t status,
-	 uint16_t PC,
-	 uint8_t SP,
-         int32_t clock) :
-	mA(A), mX(X), mY(Y), mStatus(status), mPC(PC), mSP(SP), mClock(clock) { }
+CPU::CPU(Registers reg, int32_t clock) :
+	mReg(reg), mClock(clock) { }
 
 
-bool CPU::getFlag(StatusFlah sflag) {
-	return mStatus & sflag;
+bool CPU::getFlag(StatusFlag sflag) {
+	return mReg.S & sflag;
 }
 	
-void CPU::setFlag(StatusFlag sflag) {
-	mStatus = mStatus | sflag ;
+void CPU::setFlag(StatusFlag sflag, bool value) {
+	if(value) {
+		mReg.S = mReg.S | sflag ;
+	} else {
+		clearFlag(sflag);
+	}
 }
 
 void CPU::clearFlag(StatusFlag sflag) {
-	mStatus = mStatus & ~sflag;
+	mReg.S = mReg.S & ~sflag;
+}
 
-};
-
-Cycle CPU:: getClock() {
+Cycle CPU::getClock() {
 	return mClock;
-
 }
 
 Cycle CPU::changeState() {
 	Cycle cycles = 0;
-	uint8_t opcode = mMem.get(mPC);
-
+	uint8_t opcode = mMem.get(mReg.PC);
+	uint8_t operand1 = mMem.get(mReg.PC + 1);
+	uint8_t operand2 = mMem.get(mReg.PC + 2);
+	int32_t instruction_length = 0;
+	Registers old_reg = mReg;
 	//logic
-
-	//mPC++;
+	switch(opcode) {
+		//ADC - Add With Carry
+		//immidiate: adc #oper
+		//2 bytes & 2 cycles
+		case 0x69:
+			mReg.A += operand1 + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 2;
+			instruction_length = 2;
+		break;
+		//zero page: adc oper
+		//2 bytes & 3 cycles
+		case 0x65:
+			mReg.A += mMem.get(operand1) + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 3;
+			instruction_length = 2;
+		break;
+		//zero page,X: adc oper,X
+		//2 bytes & 4 cycles
+		case 0x75:
+			mReg.A += mMem.get(operand1 + mReg.X) + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 4;
+			instruction_length = 2;
+		break;
+		//Absolute: adc oper
+		//3 bytes & 4 cycles
+		case 0x60:
+			mReg.A += mMem.get(operand1 + operand2*0x100) + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 4;
+			instruction_length = 3;
+		break;
+		//Absolute,X: adc oper,X
+		//3 bytes & 4(+1 if page boundary exceeded) cycles
+		case 0x70:
+			mReg.A += mMem.get(operand1 + operand2*0x100 + mReg.X) + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 4 + (operand1 + mReg.X) > 0xff;
+			instruction_length = 3;
+		break;
+		//Absolute,Y: adc oper,Y
+		//3 bytes & 4(+1 if page boundary exceeded) cycles
+		case 0x79:
+			mReg.A += mMem.get(operand1 + operand2*0x100 + mReg.Y) + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 4 + (operand1 + mReg.Y) > 0xff;
+			instruction_length = 3;
+		break;
+		//(Indirect,X): adc (oper,X)
+		//2 bytes & 6 cycles 
+		case 0x61:
+			mReg.A += mMem.get(mMem.get(operand1 + mReg.X)) + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 6;
+			instruction_length = 2;
+		break;
+		//(Indirect),X: adc (oper),X
+		//2 bytes & 5 cycles
+		case 0x71:
+			mReg.A += mMem.get(mMem.get(operand1) + mReg.X) + getFlag(CARRY);
+			setFlag(static_cast<StatusFlag>(CARRY|OVERFLOW), mReg.A < old_reg.A);
+			setFlag(static_cast<StatusFlag>(ZERO), !mReg.A);
+			cycles = 5 + (mMem.get(operand1) + mReg.X) > 0xff;
+			instruction_length = 2;
+		break;
+		default:
+			std::cout << static_cast<int32_t>(opcode)
+			 	<< " is not a legal opcode."
+				<< " PC will be incremented by one byte"
+				<< std::endl;
+			instruction_length = 1;
+	}
+	
+	mReg.PC += instruction_length;
 	return cycles;
 }
-
--- src/NES/cpu.hpp
+++ src/NES/cpu.hpp
@@ -1,7 +1,7 @@
 //implementation of 6502
 #include <stdint.h>
 
-enum StatusFlag= { 	CARRY = 0x01,
+enum StatusFlag { 	CARRY = 0x01,
 			ZERO = 0x02,
 			INTERRUPT = 0x04,
 			DECIMAL_MODE = 0x08,
@@ -9,31 +9,31 @@ enum StatusFlag= { 	CARRY = 0x01,
 			//ALWAYS_ONE = 0x20,
 			OVERFLOW = 0x40,
 			SIGN= 0x80 };
-			
+
+struct Registers {
+	uint8_t A;
+	uint8_t X;
+	uint8_t Y;
+	uint8_t S;
+	uint16_t PC;
+	uint8_t SP;
+};
+	
 typedef int32_t Cycle;
 
 class CPU {
 	public:
-		CPU(	uint8_t A,
-			uint8_t X,
-			uint8_t Y,
-			uint8_t status,
-			uint16_t PC,
-			uint8_t SP,
-			int32_t clock);
+		CPU(Registers reg, Cycle clock);
 		bool getFlag(StatusFlag sflag);
-		void setFlag(StatusFlag sflag, bool value);
+		void setFlag(StatusFlag sflag, bool value = 1);
+		void clearFlag(StatusFlag sflag);
 		//do we even need these
-		int32_t getClock();
+		Cycle getClock();
 		Cycle changeState();
 
 	private:
-		uint8_t mA;
-		uint8_t mX;
-		uint8_t mY;
-		uint8_t mStatus;
-		uint16_t mPC;
-		uint8_t mSP;
+		Registers mReg;
 		Cycle mClock;
 		Memory mMem;
+
 };
