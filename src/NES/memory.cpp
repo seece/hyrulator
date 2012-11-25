819593139795ea8addeeb256565bafb8df0db92d
Pekka Väänänen <pekka.vaananen@iki.fi>
2012-11-25 23:31:24 +0200

    Fix memory datatypes

--- src/NES/memory.cpp
+++ src/NES/memory.cpp
@@ -1,19 +1,20 @@
 
 #include <stdint.h>
 #include <iostream>
+#include <cstring>
 #include "memory.hpp"
 
 Memory::Memory() {
 	// in the NES the memory is not zeroed and this might mask some bugs
-	memset(mRam, 0, MEM_SIZE);
+	memset(mRam, 0, ram_size);
 }
 
-uint8_t Memory::read(uint8_t offset) {
+uint8_t Memory::read(uint32_t offset) {
 	// TODO add here some mapper processing
 	return mRam[offset];
 }
 
-void Memory::write(uint8_t offset, uint8_t value) {
+void Memory::write(uint32_t offset, uint8_t value) {
 	// TODO add here some mapper processing too
 	mRam[offset] = value;
 }
--- src/NES/memory.hpp
+++ src/NES/memory.hpp
@@ -2,16 +2,18 @@
 
 #include <stdint.h>
 
-#define RAM_SIZE 2048
+const uint32_t ram_size= 2048;
 
 class Memory {
 	
+
+
 	public:
 		Memory(); // placeholder, should take arguments
 		uint8_t read(uint32_t offset);
 		void write(uint32_t offset, uint8_t value);
 	private:
 		// includes zero page, stack and RAM
-		uint8_t mRam[RAM_SIZE];
+		uint8_t mRam[ram_size];
 };
 
